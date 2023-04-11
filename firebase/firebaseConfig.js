18ec43560eb1b19b1ef97f921d373a4dec64592b
sriman001 <110296786+sriman001@users.noreply.github.com>
2023-04-11 22:17:03 +0530

    Add files via upload

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig={
+    apiKey: "AIzaSyBwBulnyxH_sVw85zNmFWhqdRkWunKpcmE",
+  authDomain: "cloudmodelexp.firebaseapp.com",
+  databaseURL: "https://cloudmodelexp-default-rtdb.firebaseio.com",
+  projectId: "cloudmodelexp",
+  storageBucket: "cloudmodelexp.appspot.com",
+  messagingSenderId: "649872746597",
+  appId: "1:649872746597:web:d238cc4ecd3f9661027e8c"
+};
+
+firebase.initializeApp(firebaseConfig);
