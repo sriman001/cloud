1b1376f8184cba38afc553ffd3cdfcc38e57863b
sriman001 <110296786+sriman001@users.noreply.github.com>
2023-04-25 10:11:00 +0530

    Delete dropbox directory

--- dropbox/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const {join}=require("path");
-app.get('/',(req,res)=>{
-    res.sendFile(join(__dirname + "/home.html"));
-});
-
-app.listen(5000);
